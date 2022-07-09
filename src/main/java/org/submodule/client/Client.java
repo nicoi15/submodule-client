aaeee959f219fafefcbb89c6a372fb436bbac98b
Nico Abrasaldo <nicoiabrasaldo@yahoo.com>
2022-07-10 00:53:49 +0800

    Added implementation

--- src/main/java/org/submodule/client/Client.java
+++ src/main/java/org/submodule/client/Client.java
@@ -1,7 +1,13 @@
 package org.submodule.client;
 
+import org.submodule.api.ApiConfig;
+
 public class Client {
     public static void main(String[] args) {
         System.out.println("Hello from Client!");
+        var apiConfig = new ApiConfig();
+        apiConfig.setName("Api");
+        apiConfig.setValue("Value");
+        System.out.println("Api Name : " + apiConfig.getName() + " Value : " + apiConfig.getValue());
     }
 }
